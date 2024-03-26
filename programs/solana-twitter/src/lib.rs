9c0f9243fc492a3d0f40ac1cdbaf012c92919aa8
Rafael Moreira <rafamsilva@gmail.com>
2024-03-26 17:52:19 +1100

    Implementation of SendTweet Context. See comments for details in each property.

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -2,18 +2,28 @@ use anchor_lang::prelude::*;
 
 declare_id!("DhKx558vL1z4NhcPYVXnYKe9C5BJiqYtijDpVqFTArp3");
 
-#[program]
-pub mod solana_twitter {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
-}
+// #[program]
+// pub mod solana_twitter {
+//     use super::*;
+//     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+//         Ok(())
+//     }
+// }
 
 #[derive(Accounts)]
-pub struct Initialize {}
-
+pub struct SendTweet<'info> {
+  /* $[account(init)] indicates we are creating an account when sending a tweet
+  and howing who's paying for the transaction and how much storage it's using */
+  #[account(init, payer = author, space = Tweet::LEN)]
+  pub tweet: Account<'info, Tweet>,
+  /* making the author account mutable, because their tweet will change how much
+  money they have in their account */
+  #[account(mut)]
+  pub author: Signer<'info>,
+  /* Type of account called Program and passing it the System type to ensure
+  it is the official System program. To avoid malicious action from outside */
+  pub system_program: Program<'info, System>,
+}
 
 /* Every tweet will have an account, which facilitates charging as every
 tweet from a account owner will be charged it's storage in it's limit of chars \
