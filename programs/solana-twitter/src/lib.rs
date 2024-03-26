90f0287ca5edc7263d5c3767f31269176ee73858
Rafael Moreira <rafamsilva@gmail.com>
2024-03-26 16:02:02 +1100

    Implemented constants to define tweet size, which gives the RENT "cost" per account.

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -25,3 +25,18 @@ pub struct Tweet {
   pub topic: String,
   pub content: String,
 }
+
+const DISCRIMINATOR_LENGTH: usize = 8; // discriminator for every account created
+const PUBLIC_KEY_LENGTH: usize = 32; //author property size
+const TIMESTAMP_SIZE: usize = 8; //timestamp property size
+const STRING_LENGTH_PREFIX: usize = 4; // Size for each char in a String
+const TOPIC_SIZE: usize = 50 * 4; // 50 chars maximum, 200 bytes
+const CONTENT_SIZE: usize = 280 * 4; // 280 chars maximum, 1120 bytes
+
+
+// Add a constant on the Tweet account that provides its total size.
+impl Tweet {
+  const LEN: usize = DISCRIMINATOR_LENGTH +
+  PUBLIC_KEY_LENGTH + TIMESTAMP_SIZE + STRING_LENGTH_PREFIX +
+  TOPIC_SIZE + CONTENT_SIZE;
+}
