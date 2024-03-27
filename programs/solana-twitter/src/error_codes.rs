cfbb936fe4395212415d7d76b6d40cd4e8e856f6
Rafael Moreira <rafamsilva@gmail.com>
2024-03-27 12:43:54 +1100

    Development of conditional for error and acting as a ProgramError. - Added Enum for ErrorMsgs (Separate file - error_codes.rs) - Added impl inside error_codes.rs to convert error msg to ProgramError. - Minor refactoring for organization

---
+++ programs/solana-twitter/src/error_codes.rs
@@ -0,0 +1,21 @@
+use anchor_lang::prelude::*;
+
+// Acts as a ENUM CLASS to handle errors messages
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
+}
+
+
+// ProgramError conversion from ErrorCode
+impl From<ErrorCode> for ProgramError {
+  fn from(error: ErrorCode) -> Self {
+    match error {
+      ErrorCode::TopicTooLong => ProgramError::Custom(1),
+      ErrorCode::ContentTooLong => ProgramError::Custom(2),
+    }
+  }
+}
--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,3 +1,6 @@
+mod error_codes;
+
+use error_codes::ErrorCode;
 use anchor_lang::prelude::*;
 use solana_program::entrypoint::ProgramResult;
 
@@ -7,17 +10,25 @@ declare_id!("DhKx558vL1z4NhcPYVXnYKe9C5BJiqYtijDpVqFTArp3");
 pub mod solana_twitter {
     use super::*;
     pub fn send_tweet(
-      // SendTweet was used as generic type to link the context to this function
-      ctx: Context<SendTweet>,
-      // Extra arguments, only account cannot be used as argument here.
-      topic: String,
-      // This function returns a ProgramResult which can either be Ok or ProgramError
-      content: String) -> ProgramResult {
+        // SendTweet was used as generic type to link the context to this function
+        ctx: Context<SendTweet>,
+        // Extra arguments, only account cannot be used as argument here.
+        topic: String,
+        // This function returns a ProgramResult which can either be Ok or ProgramError
+        content: String
+      ) -> ProgramResult {
         // & is used to access account by reference, it's like borrowing in Rust
         let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
         // Signer is used to guarantee that the account is owned by the author
         let author: &Signer = &ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
+
+        if topic.chars().count() > 50 {
+          return Err(ErrorCode::TopicTooLong.into());
+        }
+        if content.chars().count() > 280 {
+          return Err(ErrorCode::ContentTooLong.into());
+        }
 
         tweet.author = *author.key;
         tweet.timestamp = clock.unix_timestamp;
@@ -30,17 +41,17 @@ pub mod solana_twitter {
 
 #[derive(Accounts)]
 pub struct SendTweet<'info> {
-  /* $[account(init)] indicates we are creating an account when sending a tweet
-  and howing who's paying for the transaction and how much storage it's using */
-  #[account(init, payer = author, space = Tweet::LEN)]
-  pub tweet: Account<'info, Tweet>,
-  /* making the author account mutable, because their tweet will change how much
-  money they have in their account */
-  #[account(mut)]
-  pub author: Signer<'info>,
-  /* Type of account called Program and passing it the System type to ensure
-  it is the official System program. To avoid malicious action from outside */
-  pub system_program: Program<'info, System>,
+    /* $[account(init)] indicates we are creating an account when sending a tweet
+    and howing who's paying for the transaction and how much storage it's using */
+    #[account(init, payer = author, space = Tweet::LEN)]
+    pub tweet: Account<'info, Tweet>,
+    /* making the author account mutable, because their tweet will change how much
+    money they have in their account */
+    #[account(mut)]
+    pub author: Signer<'info>,
+    /* Type of account called Program and passing it the System type to ensure
+    it is the official System program. To avoid malicious action from outside */
+    pub system_program: Program<'info, System>,
 }
 
 /* Every tweet will have an account, which facilitates charging as every
@@ -48,10 +59,10 @@ tweet from a account owner will be charged it's storage in it's limit of chars \
 */
 #[account]
 pub struct Tweet {
-  pub author: Pubkey,
-  pub timestamp:i64,
-  pub topic: String,
-  pub content: String,
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub topic: String,
+    pub content: String,
 }
 
 const DISCRIMINATOR_LENGTH: usize = 8; // discriminator for every account created
@@ -61,10 +72,12 @@ const STRING_LENGTH_PREFIX: usize = 4; // Size for each char in a String
 const TOPIC_SIZE: usize = 50 * 4; // 50 chars maximum, 200 bytes
 const CONTENT_SIZE: usize = 280 * 4; // 280 chars maximum, 1120 bytes
 
-
 // Add a constant on the Tweet account that provides its total size.
 impl Tweet {
-  const LEN: usize = DISCRIMINATOR_LENGTH +
-  PUBLIC_KEY_LENGTH + TIMESTAMP_SIZE + STRING_LENGTH_PREFIX +
-  TOPIC_SIZE + CONTENT_SIZE;
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH
+        + TIMESTAMP_SIZE
+        + STRING_LENGTH_PREFIX
+        + TOPIC_SIZE
+        + CONTENT_SIZE;
 }
